c44027a487c96e79664bd67b0c088e9fa597bdf0
FossenWang <31503932+FossenWang@users.noreply.github.com>
2018-10-03 20:57:52 +0800

    定义常量类

--- read_only/read_only.py
+++ read_only/read_only.py
@@ -1,87 +1,47 @@
 """read only atrribute"""
 
-def new_attr_setter_except(__class__, read_only_attrs=None):
-    if read_only_attrs:
-        read_only_attrs = frozenset(read_only_attrs)
-    else:
-        read_only_attrs = frozenset()
 
+class ReadOnlyDict(dict):
+    def __setitem__(self, key, value):
+        raise AttributeError("Attribute '%s' is read-only" % key)
+
+
+def new_attr_setter(cls):
     def setter(self, name, value):
-        try:
-            getattr(self, name)
-        except AttributeError:
-            return super().__setattr__(name, value)
-
-        if name not in read_only_attrs:
-            raise AttributeError("Attribute '%s' is Read Only" % name)
-        return super().__setattr__(name, value)
-
+        if name == '__dict__':
+            if isinstance(self.__dict__, ReadOnlyDict):
+                raise AttributeError("Attribute '%s' is read-only" % name)
+            # 不允许随意修改__dict__属性
+            super(cls, self).__setattr__(name, value)
+        elif name in dir(self):
+            # 不允许随意修改已有的属性
+            raise AttributeError("Attribute '%s' is read-only" % name)
+        else:
+            super(cls, self).__setattr__(name, value)
     return setter
-
-
-def new_attr_setter_all(__class__, read_only_attrs=None):
-    def setter(self, name, value):
-        try:
-            getattr(self, name)
-        except AttributeError:
-            return super().__setattr__(name, value)
-
-        raise AttributeError("Attribute '%s' is Read Only" % name)
-
-    return setter
-
-
-def new_attr_setter_include(__class__, read_only_attrs=None):
-    '返回一个setter函数，该方法即只读类的__setattr__方法，用于控制'
-    if read_only_attrs:
-        read_only_attrs = frozenset(read_only_attrs)
-    else:
-        read_only_attrs = frozenset()
-
-    def setter(self, name, value):
-        try:
-            getattr(self, name)
-        except AttributeError:
-            return super().__setattr__(name, value)
-
-        if name in read_only_attrs:
-            raise AttributeError("Attribute '%s' is Read Only" % name)
-        return super().__setattr__(name, value)
-
-    return setter
-
-
-func_map = {
-    'include': new_attr_setter_include,
-    'except': new_attr_setter_except,
-    'all': new_attr_setter_all,
-    }
-
-def new_attr_setter(__class__, read_only_attrs=None, mode='include'):
-    return func_map[mode](__class__, read_only_attrs)
 
 
 class ReadOnlyMeta(type):
     def __new__(cls, name, bases, attrs):
+        '修改类方法__setattr__, 限制修改条件'
         new_cls = type.__new__(cls, name, bases, attrs)
-
-        read_only_attrs = attrs.get('read_only_attrs', None)
-        args = [new_cls, read_only_attrs]
-        if 'read_only_mode' in attrs:
-            args.append(attrs['read_only_mode'])
-
-        new_cls.__setattr__ = new_attr_setter(*args)
+        new_cls.__setattr__ = new_attr_setter(new_cls)
         new_cls.__setattr__.__module__ = attrs.get('__module__')
         new_cls.__setattr__.__qualname__ = '__setter__'
         return new_cls
 
 
 class ReadOnly(metaclass=ReadOnlyMeta):
-    pass
+    def __new__(cls, **kwargs):
+        '修改实例字典__dict__, 限制修改实例字典'
+        instance = super().__new__(cls, **kwargs)
+        instance.__dict__ = ReadOnlyDict(instance.__dict__)
+        return instance
 
 
 class Constant(ReadOnly):
-    read_only_mode = 'all'
+    pass
+
 
 # global constant
 c = Constant()
--- read_only/tests.py
+++ read_only/tests.py
@@ -1,30 +1,34 @@
 from unittest import TestCase
 
-from .read_only import ReadOnly, c
+from .read_only import c
 
-class A(ReadOnly):
-    read_only_attrs = {'x', 'z'}
-    z = 3
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
 
-c.TEST = True
-print(c.TEST, dir(c))
-# c.read_only_mode = 'asd'
 
-# c.TEST = False
-
-# python -m unittest discover
 class ReadOnlyTest(TestCase):
     def test_read_only(self):
-        a1 = A(1, 2)
-        self.assert_obj_of_A(a1)
-        a2 = A(1, 2)
-        self.assert_obj_of_A(a2)
+        self.assertDictEqual(c.__dict__, {})
 
-    def assert_obj_of_A(self, obj):
-        def assignment():
-            obj.x = 1
-        self.assertRaises(AttributeError, assignment)
-        obj.y = 100
+        # 首次赋值
+        c.test = 'test'
+        self.assertEqual(c.test, 'test')
+        self.assertDictEqual(c.__dict__, {'test': 'test'})
+
+        # 不允许再次赋值
+        def func():
+            c.test = 'test'
+        self.assertRaises(AttributeError, func)
+
+        # 不允许通过__dict__修改属性
+        def func():
+            c.__dict__['test'] = '123'
+        self.assertRaises(AttributeError, func)
+
+        # 不允许修改__dict__
+        def func():
+            c.__dict__ = {'test': 'zxc'}
+        self.assertRaises(AttributeError, func)
+
+        # 不允许修改__setter__
+        def func():
+            c.__setattr__ = None
+        self.assertRaises(AttributeError, func)
